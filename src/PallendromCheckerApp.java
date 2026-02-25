1e7c2985b8a01c18433a82c8980a0c5b97e574f1
devashyyaakrish <devashyakothari@gmail.com>
2026-02-25 11:16:12 +0530

    UC1 printing hello message

---
+++ src/PallendromCheckerApp.java
@@ -0,0 +1,8 @@
+public class PallendromCheckerApp {
+    public static void main(String[] var0) {
+      System.out.println("=== Palindrome Checker App ===");
+      System.out.println("Version 1.0 - Complete Implementation");
+      System.out.println("Welcome Message");
+   }
+    
+}
